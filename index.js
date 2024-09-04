b3892acc28abea079c776e7ac9bd1188f00441a7
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-09-04 18:01:28 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 //CONECTAR MONGODB CON NODE
 
-'use strict' //modo estricto de JS
+"use strict"; //modo estricto de JS
 
-const mongoose = require('mongoose') //cargar modulo de mongoose
-const app = require('./app')
-const port = 3900
+const mongoose = require("mongoose"); //cargar modulo de mongoose
+const app = require("./app");
+const port = 3900;
 
 //
-const uri = 'mongodb+srv://miguelangelfb19:65d26327d2@blog.tq5vo.mongodb.net/api-rest-blog?retryWrites=true&w=majority&appName=blog'
+const uri =
+  "mongodb+srv://miguelangelfb19:65d26327d2@blog.tq5vo.mongodb.net/api-rest-blog?retryWrites=true&w=majority&appName=blog";
 // const localUri = 'mongodb://localhost:27017/api-rest-blog'
 
-mongoose.Promise = global.Promise //uso de promesas para evitar fallos a la hora de conectarnos (funionamiento interno de mongoose)
-                                /*URL mongoDB*/             
-mongoose.connect(uri)
-        .then(()=>{
-            console.log('La conexión a la base de datos fue realizada con éxito')
+mongoose.Promise = global.Promise; //uso de promesas para evitar fallos a la hora de conectarnos (funionamiento interno de mongoose)
+/*URL mongoDB*/
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("La conexión a la base de datos fue realizada con éxito");
+  })
+  .catch((err) => {
+    console.error("Error al conectar con la base de datos", err);
+  }); //conexion a mongoDB
 
-            //Crear servidor y escuchar peticiones HTTP
-            app.listen(port, () => {
-                console.log(`Servidor corriendo en http://localhost:${port}`)
-            })
-}) //conexion a mongoDB
-
-
-
-
+module.exports = app;
