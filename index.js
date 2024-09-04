17e33349bb4fb8864dd07f9dcfba9b8a15325b38
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-09-04 16:17:21 -0500

    update moongo atlas conection

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const mongoose = require('mongoose') //cargar modulo de mongoose
 const app = require('./app')
 const port = 3900
 
+//
+const uri = 'mongodb+srv://miguelangelfb19:65d26327d2@blog.tq5vo.mongodb.net/api-rest-blog?retryWrites=true&w=majority&appName=blog'
+// const localUri = 'mongodb://localhost:27017/api-rest-blog'
+
 mongoose.Promise = global.Promise //uso de promesas para evitar fallos a la hora de conectarnos (funionamiento interno de mongoose)
                                 /*URL mongoDB*/             
-mongoose.connect('mongodb://localhost:27017/api-rest-blog')
+mongoose.connect(uri)
         .then(()=>{
             console.log('La conexión a la base de datos fue realizada con éxito')
 
