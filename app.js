3f0195712aa0d16b75dbcc34e20e865afc2c00dd
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-09-04 19:04:24 -0500

    ccors configuration

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
-'use strict'
+"use strict";
 
 //CREAR NUESTRO SERVIDOR WEB
 
 //Cargar modulos de Node para crear servidor
 
-let express = require('express')
-let bodyParser = require('body-parser')
-const cors = require('cors');
-
+let express = require("express");
+let bodyParser = require("body-parser");
+const cors = require("cors");
 
 //Ejecutar Express para tabajar con HTTP
 
-let app = express()
+let app = express();
 
 //Cargar ficheros rutas
 
-let article_routes = require('./routes/article')
+let article_routes = require("./routes/article");
 
 //Cargar MiddLewares
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
 const allowedOrigins = ["https://blog-react-kohl.vercel.app"]; // Agrega tu frontend aquí
 app.use(
   cors({
     origin: allowedOrigins,
+    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Cargar CORS (para permitir peticiones desde el FrontEnd)
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 //Añadir prefijos a las rutas / cargar rutas
-app.use('/api', article_routes)
+app.use("/api", article_routes);
 
+//Exportar modulo (fichero actual)
 
-//Exportar modulo (fichero actual) 
-
-module.exports = app
+module.exports = app;
