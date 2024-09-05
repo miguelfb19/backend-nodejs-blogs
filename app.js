dc08b6da9667b269690b961825ec9c81cde4cc2d
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-09-05 09:18:26 -0500

    new url to acces

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let article_routes = require("./routes/article");
 
 //Cargar MiddLewares
 
-const allowedOrigins = ["https://blog-react-kohl.vercel.app", "https://blog-vuejs-alpha.vercel.app"]; // Agrega tu frontend aquí
+const allowedOrigins = ["https://blog-react-kohl.vercel.app", "https://blog-vuejs-alpha.vercel.app", "http://localhost:3000"]; // Agrega tu frontend aquí
 app.use(
   cors({
     origin: function (origin, callback) {
