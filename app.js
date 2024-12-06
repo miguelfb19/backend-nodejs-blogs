a3222dd7205f5a1d8b5694e4bf1e75ec42c5f8ae
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-12-06 15:44:12 -0500

    little update

--- app.js
+++ app.js
@@ -18,17 +18,24 @@ let article_routes = require("./routes/article");
 
 //Configuración de CORS
 
-const allowedOrigins = ["https://blog-angular-sigma.vercel.app", "https://blog-react-kohl.vercel.app", "https://blog-vuejs-alpha.vercel.app","http://localhost:5173", "http://localhost:3000", "http://localhost:4200"]; // Agrega tu frontend aquí
+const allowedOrigins = [
+  "https://blog-angular-sigma.vercel.app",
+  "https://blog-react-kohl.vercel.app",
+  "https://blog-vuejs-alpha.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "http://localhost:4200",
+]; // Agrega tu frontend aquí
 app.use(
   cors({
     origin: function (origin, callback) {
-        if (!origin) return callback(null, true); // Permite solicitudes sin origen como las de Postman
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg = "El origen CORS no está permitido.";
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
+      if (!origin) return callback(null, true); // Permite solicitudes sin origen como las de Postman
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = "El origen CORS no está permitido.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
